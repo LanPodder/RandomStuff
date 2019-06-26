19c4f0bfbb7a9df822f114a52d74960ee0aab824
LanPodder <35526460+LanPodder@users.noreply.github.com>
2019-06-26 13:21:17 +0200

    fixed errors in DateStuff

--- RandomStuff/src/DateStuff/DateFormatting.java
+++ RandomStuff/src/DateStuff/DateFormatting.java
@@ -3,16 +3,15 @@ package DateStuff;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.*;
+import java.util.Locale;
 
 public class DateFormatting {
 	public static void main(String[] args) {
 		//printTimeNow();
 		//printSpecifiedDateNoFormat("2019-06-26");
 		
-		DateTimeFormatterBuilder formatBuilder = new DateTimeFormatterBuilder();
-		formatBuilder.appendPattern("dd.MM.yyyy");
-		parseSpecifiedDateInFormat("26.06.2019", formatBuilder.toFormatter());
-		
+
 		printCurrentFormattedDateTime(LocalDateTime.now(), "e M yyyy");
 		printCurrentFormattedDateTime(LocalDateTime.now(), "ee MM yyyy");
 		printCurrentFormattedDateTime(LocalDateTime.now(), "eee MMM yyyy");
@@ -20,12 +19,26 @@ public class DateFormatting {
 		printCurrentFormattedDateTime(LocalDateTime.now(), "eeeee MMMMM yyyy");
 		printCurrentFormattedDateTime(LocalDateTime.now(), "d MMMM yyyy");
 		printCurrentFormattedDateTime(LocalDateTime.now(), "dd MMMM yyyy");
+		
+		DateTimeFormatterBuilder formatBuilder = new DateTimeFormatterBuilder();
+		formatBuilder.appendPattern("dd.MM.yyyy");
+		LocalDate twentyFifth = parseSpecifiedDateInFormat("25.05.2019", formatBuilder.toFormatter(Locale.getDefault()));
+		
+		printCurrentFormattedDate(twentyFifth, "dd MMMM yyyy");
+		
 	}
 	
 	public static void printCurrentFormattedDateTime(LocalDateTime dateTime, String pattern) {
 		DateTimeFormatterBuilder formatBuilder  = new DateTimeFormatterBuilder();
 		formatBuilder.appendPattern(pattern);
 		String formattedDateString = dateTimeToFormattedString(dateTime, formatBuilder.toFormatter());
+		System.out.println(formattedDateString);
+	}
+	
+	public static void printCurrentFormattedDate(LocalDate dateTime, String pattern) {
+		DateTimeFormatterBuilder formatBuilder  = new DateTimeFormatterBuilder();
+		formatBuilder.appendPattern(pattern);
+		String formattedDateString = dateToFormattedString(dateTime, formatBuilder.toFormatter());
 		System.out.println(formattedDateString);
 	}
 	
@@ -50,15 +63,19 @@ public class DateFormatting {
 		System.out.println(dateTime);
 	}
 	
-	public static LocalDateTime parseSpecifiedDateNoFormat(String dateString) {
-		return LocalDateTime.parse(dateString); //no specified format -> ISO_LOCAL_DATE format e.g. 2019-06-26
+	public static LocalDate parseSpecifiedDateNoFormat(String dateString) {
+		return LocalDate.parse(dateString); //no specified format -> ISO_LOCAL_DATE format e.g. 2019-06-26
 	}
 	
-	public static LocalDateTime parseSpecifiedDateInFormat(String dateString, DateTimeFormatter format) {
-		return LocalDateTime.parse(dateString, format); //takes a string in "format" format, then parses it into ISO_LOCAL_DATE format
+	public static LocalDate parseSpecifiedDateInFormat(String dateString, DateTimeFormatter format) {
+		return LocalDate.parse(dateString, format); //takes a string in "format" format, then parses it into ISO_LOCAL_DATE format
 	}
 	
 	public static String dateTimeToFormattedString(LocalDateTime date, DateTimeFormatter format) {
+		return date.format(format);
+	}
+	
+	public static String dateToFormattedString(LocalDate date, DateTimeFormatter format) {
 		return date.format(format);
 	}
 }
